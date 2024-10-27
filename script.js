75320ecf401b845a6b75986fb96fe8e0b0c5ceaf
david <davudseft@gmail.com>
2024-10-27 05:40:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,13 @@ document.getElementById('textForm').addEventListener('submit', async function(ev
     
     const text = document.getElementById('textInput').value;
 
-    // تابع برای بدست آوردن SHA فایل موجود
-    async function getFileSHA() {
-        const response = await fetch('https://api.github.com/repos/davudsedft/admin/contents/textfile.txt', {
-            method: 'GET',
-            headers: {
-                'Authorization': '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'
-            }
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            return data.sha;
-        } else {
-            throw new Error('Failed to retrieve SHA');
-        }
-    }
-
-    let sha;
-    try {
-        sha = await getFileSHA();
-    } catch (error) {
-        console.error(error);
-        document.getElementById('response').innerText = 'Failed to retrieve SHA.';
-        return;
-    }
+    // SHA فایل موجود
+    const sha = '8b137891791fe96927ad78e64b0aad7bded08bdc';
     
     const response = await fetch('https://api.github.com/repos/davudsedft/admin/contents/textfile.txt', {
         method: 'PUT',
         headers: {
-            'Authorization': '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',
+            'Authorization': 'token YOUR_GITHUB_TOKEN',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
