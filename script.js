95bc7051ad5039aa281985da2e7f642279d32965
david <davudseft@gmail.com>
2024-10-27 05:42:43 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('textForm').addEventListener('submit', async function(ev
     const response = await fetch('https://api.github.com/repos/davudsedft/admin/contents/textfile.txt', {
         method: 'PUT',
         headers: {
-            'Authorization': 'token YOUR_GITHUB_TOKEN',
+            'Authorization': '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',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
